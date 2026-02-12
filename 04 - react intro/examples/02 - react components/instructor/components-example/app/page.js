4757405d2e095265a7806017208d148abc9c413a
Oliver Antoniu <oliver+github@abolish.golf>
2026-02-12 10:31:06 -0700

    add [04 - react]: ex02 - implement a static component

--- 04 - react intro/examples/02 - react components/instructor/components-example/app/page.js
+++ 04 - react intro/examples/02 - react components/instructor/components-example/app/page.js
@@ -1,10 +1,15 @@
+// styles
 import styles from "./page.module.css";
+
+// components
+import Hello from "./components/Hello.jsx";
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-
+        <Hello />
       </main>
     </div>
   );
