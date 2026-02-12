38a0ae9c6e3f437d4c077b1385367deda8c6e95b
Oliver Antoniu <oliver+github@abolish.golf>
2026-02-12 10:43:00 -0700

    add [04 - react]: ex02 - implement component wrapping with children prop

--- 04 - react intro/examples/02 - react components/instructor/components-example/app/page.js
+++ 04 - react intro/examples/02 - react components/instructor/components-example/app/page.js
@@ -2,6 +2,7 @@
 import styles from "./page.module.css";
 
 // components
+import ComponentWrapper from "./components/ComponentWrapper"; // you also don't *need* to specify component file extensions!
 import Hello from "./components/Hello.jsx";
 import NewConcept from "./components/NewConcept.jsx";
 
@@ -11,10 +12,12 @@ export default function Home() {
     <div className={styles.page}>
       <main className={styles.main}>
         <Hello />
-        {/* We can reuse our components; that's the whole point! */}
-        <NewConcept concept="creating components" />
-        <NewConcept concept="passing props as data to components" />
-        <NewConcept concept="a little bit about machine learning and how neural nets work (oops)" />
+        <ComponentWrapper textColor="blue">
+          {/* We can reuse our components; that's the whole point! */}
+          <NewConcept concept="creating components" />
+          <NewConcept concept="passing props as data to components" />
+          <NewConcept concept="a little bit about machine learning and how neural nets work (oops)" />
+        </ComponentWrapper>
       </main>
     </div>
   );
